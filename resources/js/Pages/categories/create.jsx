56f99e837045492aa6d4f7e3fc404c83fec5ed51
anu398 <anuthalayad@gmail.com>
2026-04-18 11:02:10 +0400

    second commit

---
+++ resources/js/Pages/categories/create.jsx
@@ -0,0 +1,8 @@
+export default function CreateCategory() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>Create Category Page</h1>
+            <p>If you see this, React routing is working </p>
+        </div>
+    );
+}
